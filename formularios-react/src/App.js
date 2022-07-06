792588c8789365a4f4a14d8f8c9c952af01088ea
71M-M414 <guilherme.vtm2@gmail.com>
2022-07-06 19:40:10 -0300

    Formulários em react

--- formularios-react/src/App.js
+++ formularios-react/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyForm from './components/MyForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h2>Formulários</h2>
+    <MyForm user = {{name:"Fulano",email:"Fulano@gmail.com",bio:"Sou estudante de psicologia", role:"user"}}/>
+   </div>
   );
 }
 
